d2e4ec590aa91f0edcbbbbb0ff520443cad73393
Jonathan Barone <johnito.barone@gmail.com>
2021-02-18 10:04:38 +0800

    updated url

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,3 +1,4 @@
-export const discogsURL = 'http://localhost:5001'
+// export const discogsURL = 'http://localhost:5001'
 export const cognitoURL = 'https://vinylscanner.auth.us-east-2.amazoncognito.com'
-export const baseURL = 'http://localhost:5000'
+// export const baseURL = 'http://localhost:5000'
+export const baseURL = 'https://api-vinylscanner.herokuapp.com'
